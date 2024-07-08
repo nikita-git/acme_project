70f2bfd60edfbae2af56805d0b3b1566b53c1718
nikita-git <nikita.maximov@gmail.com>
2024-07-08 16:32:30 +0300

    Изменили навигацию на главной странице.

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -5,10 +5,25 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+# Для создания формы регистрации нового пользователя.
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+# К импортам из django.urls добавьте импорт функции reverse_lazy
+from django.urls import include, path, reverse_lazy
+
 
 urlpatterns = [
     # Подключаем urls.py приложения для работы с пользователями.
     path('auth/', include('django.contrib.auth.urls')),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('pages:homepage'),
+        ),
+        name='registration',
+    ),
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('birthday/', include('birthday.urls')),
